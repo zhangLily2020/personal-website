1129c55d47ddf96d9de48fc895d46f22bbf34f90
zhangLily2020 <zhangyuzhelily@gmail.com>
2024-12-29 22:53:21 -0500

    dist files

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/personal-website/',
+  build: {
+    outDir: './', // Output files to the root directory
+  },
 })
 
 
